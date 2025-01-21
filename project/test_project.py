4bf77bbd11e38febfb366d3d844312c8e61af8d3
Arthur Hough <hough1ae@gmail.com>
2025-01-21 09:19:41 -0700

    update sample historical data

--- project/test_project.py
+++ project/test_project.py
@@ -1,4 +1,17 @@
 import pytest
+import plotly.graph_objects as go
+from plotly.subplots import make_subplots
+from alpaca.data import StockHistoricalDataClient
+from alpaca.trading.client import TradingClient
+from alpaca.data.requests import StockBarsRequest
+from alpaca.data.timeframe import TimeFrame, TimeFrameUnit
+import pandas as pd
+import os
+from dotenv import load_dotenv
+from datetime import datetime, timedelta
+from zoneinfo import ZoneInfo
+import time
+import sys
 
 
 def test_function_1():
